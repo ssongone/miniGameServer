8e1b3b1d5922234aaf197dc190c8a426ed668f98
ssongone <41146676+ssongone@users.noreply.github.com>
2023-05-27 13:47:10 +0900

    Feat: while문 추가

--- nioClient/src/main/java/org/example/Main.java
+++ nioClient/src/main/java/org/example/Main.java
@@ -9,33 +9,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        ByteBuffer buffer = ByteBuffer.allocate(10);
-
         try {
             // 소켓 채널 생성
             SocketChannel socketChannel = SocketChannel.open();
             socketChannel.connect(new InetSocketAddress("localhost", 12345));
 
-            buffer.put((byte)sc.nextInt());
-            String body = sc.next();
-            byte[] byteBody = body.getBytes("UTF-8");
-            buffer.put((byte) byteBody.length);
-            buffer.put(byteBody);
+            ByteBuffer buffer = ByteBuffer.allocate(10);
+            Scanner sc = new Scanner(System.in);
 
-            buffer.flip();
+            while (true) {
+                buffer.clear();
 
-            socketChannel.write(buffer);
-            //buffer.clear();
-             //소켓 채널 닫기
+                int type = sc.nextInt();
+                if (type == -1)
+                    break;
+                String body = sc.next();
+                byte[] byteBody = body.getBytes("UTF-8");
+
+
+                buffer.put((byte) type);
+                buffer.put((byte) byteBody.length);
+                buffer.put(byteBody);
+                buffer.flip();
+                socketChannel.write(buffer);
+            }
+
+            // 소켓 채널 닫기
             socketChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
-
     }
 }
